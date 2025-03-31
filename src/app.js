0288e06981c8cfdb0a453614696bd63af857019c
yayakum <yarosgomez96@gmail.com>
2025-03-30 18:35:47 -0600

    xdxd

--- src/app.js
+++ src/app.js
@@ -36,9 +36,20 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 };
-
 // Middleware
-app.use(cors(corsOptions)); // Usar las opciones de CORS configuradas
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  res.header('Access-Control-Allow-Origin', origin || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  next();
+});
 app.use(express.json()); // Para parsear el cuerpo de las solicitudes en formato JSON
 app.use(morgan('dev')); // Para logging de las solicitudes
 // Luego, agrega esta línea después de app.use(morgan('dev'));
